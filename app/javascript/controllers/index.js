c37f02c6f08027d122afd54c60344533d8812a6e
Andres <andresd.ad5@gmail.com>
2023-10-31 20:49:53 -0300

    feat: jQuery was added, and dynamic navigation using jQuery was implemented in English.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,4 @@
 // Import and register all your controllers from the importmap under controllers/*
-
 import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
@@ -9,3 +8,33 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+// $(document).ready(function() {
+//     console.log("Hola mi nombre es andres");
+// });
+
+function addSection(context)  {
+    const idOfElement = $(context).attr("id");
+    console.log("este es el this",context)
+    switch(idOfElement){
+        case "SERVICIO":
+            $('.container_main').html('ESTOY EN SERVICIO')
+            break
+        case "CITA":
+            $('.container_main').html('ESTOY EN CITA')
+            break
+        case "RESUMEN":
+            $('.container_main').html('ESTOY EN RESUMEN')
+            break
+        default:
+            $('.container_main').html('NO ESTOY EN NINGUNA OPCION')
+    }
+}
+$( document ).on( 'ready', function (e) {
+    $(".container_ul_options").children().each(function(){addSection(this)});
+    $(".container_ul_options").on("click", "li", function() {
+        console.log("me ejecuto");
+        $("li").removeClass("active");
+        $(this).addClass("active");
+        addSection(this);
+    });
+})
